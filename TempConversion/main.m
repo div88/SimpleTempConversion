6c563d492fab16848c9f393d77ed9aacc753d853
div88 <diviyengar@gmail.com>
2016-12-19 17:39:00 -0800

    SimpleTempCoversion - intial commit

--- TempConversion/main.m
+++ TempConversion/main.m
@@ -11,7 +11,24 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        int tempValue;
+        char response;
+        float convTempCel;
+        float convTempFar;
+        
+        NSLog(@"Please enter the temperature in Celsius or Farenheit");
+        scanf("%i", &tempValue);
+        
+        NSLog(@"Is temperature in farenheit? (y/n)");
+        scanf("\n%c", &response);
+        
+        if (response == 'y') {
+            convTempCel = (tempValue - 32) * 5/9;
+            NSLog(@"Farenheit value you entered is %i and Celsius Value is %f", tempValue, convTempCel);
+        } else {
+            convTempFar = (tempValue * 9/5) + 32;
+            NSLog(@"Celsius value you enterd is %i and Farenheit Value is %f", tempValue, convTempFar);
+        }
     }
     return 0;
 }
